c9b9959c15380ae8beb391563b199940fa2fecd3
MrDemonlxl <714416426@qq.com>
2021-05-17 11:11:11 +0800

    5.17 init

--- src/main/java/com/example/mybatisplus/entity/User.java
+++ src/main/java/com/example/mybatisplus/entity/User.java
@@ -1,10 +1,22 @@
 package com.example.mybatisplus.entity;
 
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
 import lombok.Data;
+
+import java.util.Date;
 
 @Data
 public class User {
-    private Integer id;
+//    @TableId(type = IdType.AUTO)  此配置配合数据库自动递增及以当前存在id加一
+    private String id;
     private String name;
+    // 字段添加填充内容
+    @TableField(fill = FieldFill.INSERT)
+    private Date gmt_create;
 
+    @TableField(fill = FieldFill.INSERT_UPDATE)
+    private Date gmt_modified;
 }
--- src/test/java/com/example/mybatisplus/MybatisplusApplicationTests.java
+++ src/test/java/com/example/mybatisplus/MybatisplusApplicationTests.java
@@ -15,12 +15,41 @@ class MybatisplusApplicationTests {
     @Autowired
     private UserMapper userMapper;
 
+    /**
+     * 查找测试
+     */
     @Test
     public void testSelect() {
         System.out.println(("----- selectAll method test ------"));
         List<User> userList = userMapper.selectList(null);
-        for(User user:userList) {
+        for (User user : userList) {
             System.out.println(user);
         }
+    }
+
+    /**
+     * 增加测试
+     */
+    @Test
+    public void testInsert() {
+        User user = new User();
+//        自定义时为自定义，无自定义时为MP自定义
+//        user.setId('12111');
+        user.setName("1111");
+
+        int result = userMapper.insert(user);
+        System.out.println(result);
+        System.out.println(user);
+
+    }
+
+
+    @Test
+    public void testUpdate() {
+        User user = new User();
+        user.setId("1394125896104075265");
+        user.setName("qqqq2222");
+        int result = userMapper.updateById(user);
+        System.out.println(result);
     }
 }
