c4029ea76962525552a334cd3d4ca580581b8b14
MrDemonlxl <714416426@qq.com>
2021-05-17 10:25:12 +0800

    Initial commit

---
+++ src/main/java/com/example/mybatisplus/entity/User.java
@@ -0,0 +1,10 @@
+package com.example.mybatisplus.entity;
+
+import lombok.Data;
+
+@Data
+public class User {
+    private Integer id;
+    private String name;
+
+}
---
+++ src/test/java/com/example/mybatisplus/MybatisplusApplicationTests.java
@@ -0,0 +1,26 @@
+package com.example.mybatisplus;
+
+import com.example.mybatisplus.entity.User;
+import com.example.mybatisplus.mapper.UserMapper;
+//import org.junit.Test;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+@SpringBootTest
+class MybatisplusApplicationTests {
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @Test
+    public void testSelect() {
+        System.out.println(("----- selectAll method test ------"));
+        List<User> userList = userMapper.selectList(null);
+        for(User user:userList) {
+            System.out.println(user);
+        }
+    }
+}
